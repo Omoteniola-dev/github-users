d0c1d952b7ef6dd6ec12c7c014643a68b7f9eae9
Teniola <fatunmbiteniola001@gmail.com>
2021-08-13 22:15:38 +0100

    fixed prop rendering from state

--- src/components/GithubUser.tsx
+++ src/components/GithubUser.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import "../App.css"
 
-const GithubUser = () => {
+interface User{
+    userData: any,
+}
+
+const GithubUser: React.FC<User> = ({ userData }) => {
+    const { avatar_url, name, followers, following, bio, public_repos, login } = userData;
     return (
-        <div>
-            
+        <div className="card">
+            <img src={avatar_url} id="img"/>
+            <section className="card-details">
+                <p id="name">{name}</p>
+                <small id="login">&lt;{login} /&gt;</small>
+                <p id={bio}>{bio}</p>
+            </section>
+            <section className="stats">
+                <p id="followers">Followers <br/>{followers}</p>
+                <p id="following">Following <br />{following}</p>
+                <p id={public_repos}>Repos <br />{public_repos}</p>
+            </section> 
         </div>
     )
 };
 
-export default GithubUser
+export default GithubUser;
