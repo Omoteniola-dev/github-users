b08e27e21684caa0f7366edea9b73699b4d69d34
Teniola <fatunmbiteniola001@gmail.com>
2021-08-13 22:16:02 +0100

    removed unneccesary props

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 interface searchProps{
     input: any,
     SearchChange: any
+    getUser: any
 }
 
-const Search:React.FC<searchProps> = ({input, SearchChange}) => {
+const Search:React.FC<searchProps> = ({input, SearchChange, getUser}) => {
     return (
         <div>
-            <form>
+            <form onSubmit={getUser}>
                 <input type="text" name="text" value={input} id="text" placeholder="Enter username" onChange={SearchChange} />
-                <input type="submit" value="search" />
+                <button>Search</button>
             </form>
         </div>
     )
