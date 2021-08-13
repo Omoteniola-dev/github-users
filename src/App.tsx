39cb9852f131d2645acbdbf52cacc0380b46ea0c
Teniola <fatunmbiteniola001@gmail.com>
2021-08-13 22:14:31 +0100

    fixed api request function and added error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 import Search from './components/Search';
+import GithubUser from "./components/GithubUser"
 
-type MyState = {
-  input: string
+interface MyState {
+  input: string,
+  userData: any
 }
 class App extends Component<{}, MyState> {
   constructor(props: any){
     super(props);
     this.state= {
-      input: ""
-    }
+      input: "",
+      userData: []
+    };
   }
 
   SearchChange = (e: any) =>{
     this.setState({ input: e.currentTarget.value })
   }
 
+  getUser = async (e: any) => {
+    e.preventDefault();
+    try{
+      let user = await axios.get(`https://api.github.com/users/${this.state.input}`) 
+      return this.setState({ userData: user.data})
+    }
+    catch(err){
+      alert(err);
+    }
+  };
+
   render() {
-    const { input } = this.state;
+    const { input, userData } = this.state;
+    
     return (
       <div>
-        <Search input={input} SearchChange={this.SearchChange}/>
+        <Search input={input} SearchChange={this.SearchChange} getUser={this.getUser}/>
+        <GithubUser userData={userData} />
       </div>
     );
   };
